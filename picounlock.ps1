f070629ea38838e046d35e67b4565d3d8164908d
chaixshot <chai5142xshot@gmail.com>
2026-09-11 18:50:41 +0700

    pump version 1.3.1

--- picounlock.ps1
+++ picounlock.ps1
@@ -1013,7 +1013,7 @@ try {
     Write-Log $errMsg "Error"
 } finally {
     Write-Header "Exited"
-    Write-Log "Version: 1.3.0" "Info"
+    Write-Log "Version: 1.3.1" "Info"
     Write-Log ""
 
     try {
